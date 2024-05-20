e96cd0d247b5c6eead4290738becb55951e1b797
El-nene <am.nenen@duocuc.cl>
2024-05-20 11:07:53 -0400

    Update saludo.py

--- 006D/saludo.py
+++ 006D/saludo.py
@@ -1,4 +1,32 @@
-print("Hola toti")
-print("bienvenido al programa de python")
+suma:
 
-"hola"
+
+
+
+
+
+
+resta:
+
+numero1=float(input(" ingrse el 1er numero que desee restar "))
+numero2=float(input(" ingrese el 2do numeros que desee restar "))
+
+total=numero1-numero2
+
+print(" el resultado de la resta es: ", total)
+
+division:
+
+
+
+
+
+
+multiplicacion:
+
+
+
+
+
+
+
